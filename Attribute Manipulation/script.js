c92b36753054e426188dbe0557933a6abe21f746
Nouman222 <noumanahmad.info@gmial.com>
2026-01-15 10:57:00 +0500

    first commit

---
+++ Attribute Manipulation/script.js
@@ -0,0 +1,18 @@
+// let width = prompt("enter image width with px")
+// let hight = prompt("enter image hight with px")
+// alert("Are you sure to change width and Hight")
+
+// let img = document.getElementById("myImage")
+// img.setAttribute("width",width)
+// img.setAttribute("hight",hight)
+
+        
+// let userWidth = prompt("Enter the width (in pixels):");
+// let userHeight = prompt("Enter the height (in pixels):");
+
+// let img = document.getElementById('myImage');
+
+// if (userWidth && userHeight) {
+//     img.style.width = userWidth + "px";
+//     img.style.height = userHeight + "px";
+//  }
---
+++ Element-Selector/script.js
@@ -0,0 +1,68 @@
+// Task 1: Basic Styling 
+// Create an HTML page with the following elements: 
+//  h1 
+//  p 
+//  button 
+// Apply CSS using element selectors: 
+//  Make all h1 text blue 
+//  Make all p text gray 
+//  Give all button elements a green background and white text
+// document.querySelector("h1").style.color = "blue";
+// document.querySelector("p").style.color = "gray";
+
+// let btn = document.querySelector("button");
+// btn.style.backgroundColor = "green";
+// btn.style.color = "white";
+
+// Task 2: Multiple Paragraphs 
+// Create at least 3 paragraphs. 
+//  Change the font size of all p elements to 18px 
+//  Change the line height of all p elements to 1.5 
+// document.querySelector("p").style.fontSize = "18px";
+// document.querySelector("p").style.lineHeight = "1.5";
+
+// Task 3: List Styling 
+// Create: 
+//  An unordered list (ul) with at least 5 items 
+// Using element selectors: 
+//  Remove bullets from the ul 
+//  Change the color of all li elements to dark red
+// let list =document.querySelectorAll("li")
+// for(i=0;i<list.length;i++){
+//     list[i].style.listStyleType="none"
+//     list[i].style.color="dark"
+// }
+
+// Task 4: Page Layout 
+// Add these elements: 
+//  body 
+//  h2 
+//  img 
+// Style them using element selectors: 
+//  Center all text inside body 
+//  Add a bottom margin to h2 
+//  Set a fixed width for all img elements
+// 1. Create the h2 and img elements
+// const heading = document.createElement('h2');
+// heading.innerText = 'This is Section Title';
+
+// const image = document.createElement('img');
+// image.src = 'via.placeholder.com';
+// image.alt = 'Description of image';
+
+
+// document.body.appendChild(heading);
+// document.body.appendChild(image);
+
+// document.body.style.textAlign = 'center';
+// heading.style.marginBottom = '20px';
+
+// image.style.width = '300px';
+
+
+
+
+
+
+
+
